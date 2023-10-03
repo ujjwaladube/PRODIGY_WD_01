0363e79ec2bed5ded56c11410d1bd47b92bcc4c2
ujjwaladube <127643670+ujjwaladube@users.noreply.github.com>
2023-10-03 12:01:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Add a scroll event listener to change menu style on scroll
 window.addEventListener("scroll", function() {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
